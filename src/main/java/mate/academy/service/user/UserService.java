a20d811ccb82548536a752999be08e407e572361
StoneBlood <brkalix@gmail.com>
2025-02-21 15:27:17 +0200

    implemented the method `register`

--- src/main/java/mate/academy/service/user/UserService.java
+++ src/main/java/mate/academy/service/user/UserService.java
@@ -1,4 +1,8 @@
 package mate.academy.service.user;
 
+import mate.academy.dto.user.UserRegistrationRequestDto;
+import mate.academy.dto.user.UserRegistrationResponseDto;
+
 public interface UserService {
+    UserRegistrationResponseDto register(UserRegistrationRequestDto requestDto);
 }
