b1b88e29bcc457d0d96fe939b4f23fab713171b9
StoneBlood <brkalix@gmail.com>
2025-02-21 15:27:17 +0200

    implemented the method `register`

---
+++ src/main/java/mate/academy/service/user/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package mate.academy.service.user;
+
+import lombok.RequiredArgsConstructor;
+import mate.academy.dto.user.UserRegistrationRequestDto;
+import mate.academy.dto.user.UserRegistrationResponseDto;
+import mate.academy.exception.RegistrationException;
+import mate.academy.mapper.UserMapper;
+import mate.academy.model.User;
+import mate.academy.repository.user.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    @Transactional
+    public UserRegistrationResponseDto register(UserRegistrationRequestDto requestDto) {
+        if (userRepository.existsByEmail(requestDto.getEmail())) {
+            throw new RegistrationException("User with email "
+                    + requestDto.getEmail() + " already exists.");
+        }
+        User user = userMapper.toModel(requestDto);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(User.Role.CUSTOMER);
+
+        userRepository.save(user);
+        return userMapper.toDto(user);
+    }
+}
