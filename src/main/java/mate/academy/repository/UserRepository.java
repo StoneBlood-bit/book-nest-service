6ec52b860e160c3d62cc67a29d17f051cc4fb1ab
StoneBlood <brkalix@gmail.com>
2025-02-21 14:26:00 +0200

    added UserRepository and method `existsByEmail`

--- src/main/java/mate/academy/repository/UserRepository.java
+++ src/main/java/mate/academy/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package mate.academy.repository;
 
-public interface UserRepository {
+import mate.academy.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
 }
