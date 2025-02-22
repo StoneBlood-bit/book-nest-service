e0c7c8ebff198f9eaec8e45e96e22b522222c156
StoneBlood <brkalix@gmail.com>
2025-02-22 18:44:00 +0200

    added AuthenticationService, CustomUserDetailsService, JwtUtil

--- src/main/java/mate/academy/dto/user/UserLoginResponseDto.java
+++ src/main/java/mate/academy/dto/user/UserLoginResponseDto.java
@@ -4,5 +4,10 @@ import lombok.Data;
 
 @Data
 public class UserLoginResponseDto {
+
     private String token;
+
+    public UserLoginResponseDto(String token) {
+        this.token = token;
+    }
 }
