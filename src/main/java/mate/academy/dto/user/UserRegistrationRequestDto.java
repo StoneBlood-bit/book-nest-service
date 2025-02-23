f15de8c6c500df3289fb966a393f9b0329c45d9d
StoneBlood <brkalix@gmail.com>
2025-02-23 20:15:48 +0200

    created jwt authentication

--- src/main/java/mate/academy/dto/user/UserRegistrationRequestDto.java
+++ src/main/java/mate/academy/dto/user/UserRegistrationRequestDto.java
@@ -4,14 +4,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import mate.academy.annotation.FieldMatch;
 
 @Data
-@FieldMatch(
-        first = "password",
-        second = "repeatPassword",
-        message = "Passwords do not match"
-)
 public class UserRegistrationRequestDto {
     @NotBlank
     @Email
@@ -20,9 +14,6 @@ public class UserRegistrationRequestDto {
     @Size(min = 8, max = 100)
     @NotBlank
     private String password;
-
-    @NotBlank
-    private String repeatPassword;
 
     @NotBlank
     private String firstName;
