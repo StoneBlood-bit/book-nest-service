af43307e76e50675362280fb1f907f737ff3beca
StoneBlood <brkalix@gmail.com>
2025-02-25 22:21:39 +0200

    reloaded the version of dependencies

--- src/test/java/mate/academy/ApplicationTests.java
+++ src/test/java/mate/academy/ApplicationTests.java
@@ -2,8 +2,10 @@ package mate.academy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "jwt.secret=${JWT_SECRET:default_value}")
 class ApplicationTests {
 
     @Test
