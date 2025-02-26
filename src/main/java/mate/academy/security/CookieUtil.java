805dffca9babc3c9de7b20b9ba885f3292d9121d
StoneBlood <brkalix@gmail.com>
2025-02-26 14:32:55 +0200

    added CookieUtil

---
+++ src/main/java/mate/academy/security/CookieUtil.java
@@ -0,0 +1,20 @@
+package mate.academy.security;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieUtil {
+    private static final String TOKEN_COOKIE_NAME = "token";
+    private static final int COOKIE_MAX_AGE = 30 * 24 * 60 * 60;
+
+    public void addTokenCookie(HttpServletResponse response, String token) {
+        Cookie cookie = new Cookie(TOKEN_COOKIE_NAME, token);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(COOKIE_MAX_AGE);
+        response.addCookie(cookie);
+    }
+}
