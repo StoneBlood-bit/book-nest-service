ae97445328c9ef278e9b9ced99d8c59af98a8050
StoneBlood <brkalix@gmail.com>
2025-03-09 17:03:27 +0200

    added the method `findAllBookTitles`

--- src/main/java/mate/academy/repository/book/BookRepository.java
+++ src/main/java/mate/academy/repository/book/BookRepository.java
@@ -1,9 +1,12 @@
 package mate.academy.repository.book;
 
+import java.util.List;
 import mate.academy.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
-
+    @Query("SELECT b.title FROM Book b")
+    List<String> findAllBookTitles();
 }
