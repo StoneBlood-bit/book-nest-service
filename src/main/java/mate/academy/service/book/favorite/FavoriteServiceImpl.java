d789cf1a19dddf2d797b0b1aa7015bf719e47f33
StoneBlood <brkalix@gmail.com>
2025-03-10 19:49:45 +0200

    added @Transactional annotations

--- src/main/java/mate/academy/service/book/favorite/FavoriteServiceImpl.java
+++ src/main/java/mate/academy/service/book/favorite/FavoriteServiceImpl.java
@@ -14,6 +14,7 @@ import mate.academy.model.User;
 import mate.academy.repository.book.BookRepository;
 import mate.academy.repository.user.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -47,6 +48,7 @@ public class FavoriteServiceImpl implements FavoriteService {
         userRepository.save(user);
     }
 
+    @Transactional
     @Override
     public void removeBookFromFavorite(Long bookId, Long userId) {
         Book book = bookRepository.findById(bookId).orElseThrow(
