38c2e5c5770f5912b635343b713f8fe8555e5c96
StoneBlood <brkalix@gmail.com>
2025-03-12 17:53:10 +0200

    created ShoppingCartRepository

---
+++ src/main/java/mate/academy/repository/shoppingcart/ShoppingCartRepository.java
@@ -0,0 +1,7 @@
+package mate.academy.repository.shoppingcart;
+
+import mate.academy.model.ShoppingCart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
+}
