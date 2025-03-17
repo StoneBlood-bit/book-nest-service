6934dd168d3a77c244662da81edd53314d82d706
StoneBlood <brkalix@gmail.com>
2025-03-17 22:38:26 +0200

    added h2 properties to tests and created BookRepositoryTest

--- src/test/java/mate/academy/repository/BookRepositoryTest.java
+++ src/test/java/mate/academy/repository/BookRepositoryTest.java
@@ -1,4 +1,19 @@
 package mate.academy.repository;
 
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql(
+        scripts = "classpath:database/01-create-books.sql",
+        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS
+)
+@Sql(
+        scripts = "classpath:database/02-clear-books.sql",
+        executionPhase = Sql.ExecutionPhase.AFTER_TEST_CLASS
+)
 public class BookRepositoryTest {
+
 }
---
+++ src/test/resources/database/01-create-books.sql
@@ -0,0 +1,3 @@
+insert into books (id, title) values (1, 'Harry Potter');
+
+insert into books (id, title) values (2, 'My Book');
