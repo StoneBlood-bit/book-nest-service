8a97b770edb6fcb74d5ce0527078ee5f6e2c3c0a
StoneBlood <brkalix@gmail.com>
2025-03-31 20:46:22 +0300

    added OrderControllerTest and ShoppingCartControllerTest

---
+++ src/test/resources/database/controller/order/02-clear-tables.sql
@@ -0,0 +1,11 @@
+delete from orders_books;
+
+delete from orders;
+
+delete from shopping_carts_books;
+
+delete from shopping_carts;
+
+delete from books;
+
+delete from users;
---
+++ src/test/resources/database/controller/shoppingcart/02-delete-shopping-cart.sql
@@ -0,0 +1,7 @@
+delete from shopping_carts_books;
+
+delete from shopping_carts;
+
+delete from books;
+
+delete from users;
